e6b5ed0c434e0fee3b0e8ef951caf2b3b1cae710
raedeid <raedeidalrfou12345@gmail.com>
2020-01-26 17:16:41 +0200

    solve problem

---
+++ challange/link-list/__test__/lib/link-list.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const linkList= require('../../link-list'); 
+
+describe('check the node ',()=>{
+  let ll = new linkList.Link();
+  ll.insert(100);
+  ll.insert(200); 
+  it('check if nodeclass create node ',()=>{
+    expect(ll !== null).toBeTruthy();
+  });
+  it('check if include method work ',()=>{
+    expect(ll.include(100)).toBeTruthy();
+  });
+  it('check if the convert to string method work',()=>{
+      expect(typeof(ll.convert_to_string())).toEqual('string')
+  })
+});
