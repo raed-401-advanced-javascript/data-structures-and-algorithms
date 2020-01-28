a844966b4266b31b9f4f850d989c1c77203da054
raedeid <raedeidalrfou12345@gmail.com>
2020-01-28 17:53:25 +0200

    added method

---
+++ challange/Linkedlist/__test__/lib/link-list.test.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const linkedList = require('../../link-list')
+describe('node link list', () => {
+    it('check if i can added node to the end of the list', () => {
+        let ll = new linkedList.Link();
+        ll.insert(2)
+        ll.insert(1)
+        ll.append(3)
+        expect(ll.head.next.next.data).toEqual(3);
+    })
+    it('check if i can added multiplie value in the end ', () => {
+        let ll = new linkedList.Link();
+        ll.insert(2)
+        ll.insert(1)
+        ll.append(3)
+        ll.append(4)
+        // console.log('value',ll.head.next.next.data)
+        expect(ll.head.next.next.data).toEqual(3)
+        expect(ll.head.next.next.next.data).toEqual(4)
+    })
+    it('check if i can added node before the node on the middle ', () => {
+        let ll = new linkedList.Link();
+        ll.insert(2)
+        ll.insert(1)
+        ll.insert(0)
+        ll.added_before(0.5, 1)
+        expect(ll.head.next.data).toEqual(0.5)
+        expect(ll.head.next.next.data).toEqual(1)
+    })
+    it('check if i can added value before the first value in the list ', () => {
+        let ll = new linkedList.Link();
+        ll.insert(2)
+        ll.insert(1)
+        ll.added_before(0, 1)
+        console.log(ll)
+        expect(ll.head.data).toEqual(0)
+    })
+    it('check if i can added node after middle node', () => {
+        let ll = new linkedList.Link();
+        ll.insert(2)
+        ll.insert(1)
+        ll.insert(0)
+        ll.added_after(1.5, 1)
+        expect(ll.head.next.data).toEqual(1)
+        expect(ll.head.next.next.data).toEqual(1.5)
+    })
+    it('check if i can added node after last node', () => {
+        let ll = new linkedList.Link();
+        ll.insert(2)
+        ll.insert(1)
+        ll.added_after(3, 2)
+        // console.log(ll)
+        expect(ll.head.next.next.data).toEqual(3)
+    })
+})
--- challange/Linkedlist/link-list.js
+++ challange/Linkedlist/link-list.js
@@ -1,3 +1,5 @@
+'use strict';
+const util = require('util')
   
 class Node {
     constructor(data,next= null){
@@ -38,25 +40,73 @@ class Node {
       return string_version;
     }
     append(value){
-      let flag = true  
-      while( this.size && flag ){
-        if(this.head.next === null){
-          // this.head = this.head.next
-          console.log(this.head.next)
-          this.head.next = new Node(value)
-          flag = false
-        }else{
-          console.log(this.head.data)  
-          this.head = this.head.next
-          // console.log(this.head)
-          this.size = this.size - 1 
-        }       
+      let node = new Node(value);
+      let current;
+      if(this.head === null){
+        this.head = node;
+      }else{
+        current = this.head
+        while(current.next){
+          current = current.next
+        } 
+        current.next = node
+        // console.log(current)
+        this.size ++
+      }
+    }
+    added_before(new_vlaue,target){
+      let current;
+      if(this.head == null){
+        this.head = new Node(new_vlaue)
+      }else{
+        let flag = true
+        current = this.head
+        while(current.next && flag){
+          if(current.next.data == target){
+            // console.log("hi",current.next.data)
+            current.next = new Node(new_vlaue,current.next)
+            flag = false
+          }else{
+            // console.log('test',current.next)
+            current = current.next
+          }
+        }
+        this.size++
+      }
+    }
+    added_after(new_value,target){
+      let current;
+      if(this.head == null){
+        this.head = new Node(new_value)
+      }else{
+        let flag = true
+        current = this.head 
+        while(current.next && flag){
+          if(current.next.data === target){
+            current.next.next = new Node(new_value,current.next.next)
+            flag = false
+          }else{
+            current = current.next
+          }
+        }
+        this.size++
       }
     }
   }
 
 let ll = new Link();
-ll.insert(100);
 ll.insert(200);
-ll.append(500);
-console.log(ll)
+ll.insert(100)
+ll.append(300);
+ll.added_before(50,100)
+ll.added_after(400,300)
+// ll.append(300);
+// ll.insert(500);
+// ll.insert(600);
+// console.log(Object.keys())
+console.log(util.inspect(ll, {showHidden: false, depth: null}))
+// let output = ll
+// console.log(output.head.next.next.next.next)
+
+module.exports = {Node,Link}
+
