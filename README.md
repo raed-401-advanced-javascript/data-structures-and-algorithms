393bd45133cc3a46d1c8edb0caaa75bcec537a4a
raedeid <raedeidalrfou12345@gmail.com>
2020-02-01 23:41:24 +0200

    make change

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 
 ### Links and Resources
 
-- [submission PR](https://github.com/raed-401-advanced-javascript/data-structures-and-algorithms/pull/6)
-- [ci/cd](https://github.com/raed-401-advanced-javascript/data-structures-and-algorithms/pull/6/checks?check_run_id=412240120) (GitHub Actions)
+- [submission PR](https://github.com/raed-401-advanced-javascript/data-structures-and-algorithms/pull/5)
+- [ci/cd](https://github.com/raed-401-advanced-javascript/data-structures-and-algorithms/pull/5/checks?check_run_id=420877581) (GitHub Actions)
 
 ### Setup
 
--- challange/Linkedlist/link-list.js
+++ challange/Linkedlist/link-list.js
@@ -1,80 +1,80 @@
 'use strict';
-const util = require('util')
+// const util = require('util');
   
 class Node {
-    constructor(data,next= null){
-      this.data = data ;
-      this.next = next ;
-    }
+  constructor(data,next= null){
+    this.data = data ;
+    this.next = next ;
   }
+}
   
-  class Link {
-    constructor(){
-      this.head = null;
-      this.size = 0;
-    }
-    insert(data){
-      this.head = new Node(data,this.head);
-      this.size = this.size + 1;
-    }
-    append(value){
-      let node = new Node(value);
-      let current;
-      if(this.head === null){
-        this.head = node;
-      }else{
-        current = this.head
-        while(current.next){
-          current = current.next
-        } 
-        current.next = node
-        // console.log(current)
-        this.size ++
-      }
-    }
-    added_before(new_vlaue,target){
-      let current;
-      if(this.head == null){
-        this.head = new Node(new_vlaue)
-      }else{
-        let flag = true
-        current = this.head
-        while(current.next && flag){
-          if(current.data === target){
-            current = new Node(new_vlaue,current)
-            flag = false
-          }
-          if(current.next.data == target){
-            // console.log("hi",current.next.data)
-            current.next = new Node(new_vlaue,current.next)
-            flag = false
-          }else{
-            // console.log('test',current.next)
-            current = current.next
-          }
-        }
-        this.size++
-      }
-    }
-    added_after(new_value,target){
-      let current;
-      if(this.head == null){
-        this.head = new Node(new_value)
-      }else{
-        let flag = true
-        current = this.head 
-        while(current.next && flag){
-          if(current.next.data === target){
-            current.next.next = new Node(new_value,current.next.next)
-            flag = false
-          }else{
-            current = current.next
-          }
-        }
-        this.size++
-      }
+class Link {
+  constructor(){
+    this.head = null;
+    this.size = 0;
+  }
+  insert(data){
+    this.head = new Node(data,this.head);
+    this.size = this.size + 1;
+  }
+  append(value){
+    let node = new Node(value);
+    let current;
+    if(this.head === null){
+      this.head = node;
+    }else{
+      current = this.head;
+      while(current.next){
+        current = current.next;
+      } 
+      current.next = node;
+      // console.log(current)
+      this.size ++;
     }
   }
+  added_before(new_vlaue,target){
+    let current;
+    if(this.head == null){
+      this.head = new Node(new_vlaue);
+    }else{
+      let flag = true;
+      current = this.head;
+      while(current.next && flag){
+        if(current.data === target){
+          current = new Node(new_vlaue,current);
+          flag = false;
+        }
+        if(current.next.data == target){
+          // console.log("hi",current.next.data)
+          current.next = new Node(new_vlaue,current.next);
+          flag = false;
+        }else{
+          // console.log('test',current.next)
+          current = current.next;
+        }
+      }
+      this.size++;
+    }
+  }
+  added_after(new_value,target){
+    let current;
+    if(this.head == null){
+      this.head = new Node(new_value);
+    }else{
+      let flag = true;
+      current = this.head; 
+      while(current.next && flag){
+        if(current.next.data === target){
+          current.next.next = new Node(new_value,current.next.next);
+          flag = false;
+        }else{
+          current = current.next;
+        }
+      }
+      this.size++;
+    }
+  }
+}
 
 // let ll = new Link();
 // ll.insert(2)
@@ -88,5 +88,5 @@ class Node {
 // // let output = ll
 // // console.log(output.head.next.next.next.next)
 
-module.exports = {Node,Link}
+module.exports = {Node,Link};
 
