6dad06ef809e455b78c5ed4966c78a190969a6ac
raedeid <raedeidalrfou12345@gmail.com>
2020-02-25 06:29:59 +0200

    added script

--- challange/merge.js
+++ challange/merge.js
@@ -1,8 +1,29 @@
 'use strict';
 
-function sort(left,arr){
+function sort(left,right){
     let output = [];
     let i = 0;
     let w = 0;
-     
+    let right_length = right.length;
+    let left_length = left.length
+    while(i<left_length && w < right_length){
+        if(left[i]<right[w]){
+            output.push(left[i])
+            i++
+        }else{
+            output.push(right[w])
+            w++
+        }
+    }
+    return output.concat(left.slice(i)).concat(right.slice(w))  
+}
+
+function devide(arr){
+    if(arr.length<2){
+        return arr
+    }
+    let middle  = Math.floor(arr/2)
+    let left = arr.slice(0,middle)
+    let right = arr.slice(middle)
+    return sort(devide(left),devide(right))
 }
