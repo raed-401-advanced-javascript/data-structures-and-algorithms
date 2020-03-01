0c3e45850795314ac4164353be5754354f988f69
raedeid <raedeidalrfou12345@gmail.com>
2020-03-01 17:08:12 +0200

    solve bug

--- challange/hashmap/repeated-word.js
+++ challange/hashmap/repeated-word.js
@@ -76,15 +76,19 @@ function first_repeate(str){
     let hash = new Hashtable(1024)
     for(let i=0;i<str.length;i++){
         if(str[i]==' '){
-            console.log(input_concat)
-            hash.set(input_concat)
-            input_concat = ''
+            // console.log(input_concat)
+            if(hash.map[hash.hash(input_concat.toLowerCase())]){
+                return input_concat.toLowerCase()
+            }else{
+                hash.set(input_concat.toLowerCase())
+                input_concat = ''
+            }
         }else{
             input_concat = input_concat + `${str[i]}`
         }
     }
 } 
-console.log(first_repeate("Once upon a time, there was a brave princess who..."))
+console.log(first_repeate("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only..."))
 // let h_t = new Hashtable(1024)
 // h_t.set('name','ahmad')
 // h_t.set('name','raed')
